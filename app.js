7167712c1581782c3348e006496d850c3ea485d8
Israel Camargo <isfelipecm@gmail.com>
2021-12-06 14:50:54 -0500

    enviroment var

--- app.js
+++ app.js
@@ -13,18 +13,17 @@ const transactionRoutes = require("./routes/transactions");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+require("dotenv").config();
 
 // Configuracion de la conexion a la bd
 mongoose
-  .connect(
-    "mongodb+srv://dbUser:blogify123@cluster0.3p7cj.mongodb.net/dbUser?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("Estamos conectados a nuestra BD");
-  })
-  .catch(() => {
-    console.log("Houston tenemos un problema");
-  });
+	.connect(process.env.DB_CONNECT)
+	.then(() => {
+		console.log("Estamos conectados a nuestra BD");
+	})
+	.catch(() => {
+		console.log("Houston tenemos un problema");
+	});
 
 app.use("/api/clients", clientRoutes);
 app.use("/api/users", userRoutes);
